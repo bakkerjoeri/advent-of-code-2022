27fe086819f1c7715760c3bf292b4add92ef3170
bakkerjoeri <bakkerjoeri@gmail.com>
2022-12-04 23:28:19 +0100

    some tweaks

--- day-01-calorie-counting/src/main.rs
+++ day-01-calorie-counting/src/main.rs
@@ -5,12 +5,11 @@ fn main() {
     let groups = input.split("\n\n");
     let groups_by_line = groups.map(|group| group.lines());
     let inventories = groups_by_line.map(|group| group.map(|line| line.parse::<i32>().unwrap()));
-    let totaled_inventories = inventories.map(|inventory| inventory.reduce(|sum, item| sum + item));
-    let biggest_total =
-        totaled_inventories.reduce(|largest, item| if item > largest { item } else { largest });
+    let totaled_inventories = inventories.map(|inventory| inventory.sum::<i32>());
+    let biggest_total = totaled_inventories.max().unwrap();
 
     println!(
         "The elf who's inventory contains the most calories is carrying {:?} calories",
-        biggest_total.unwrap().unwrap()
-    )
+        biggest_total
+    );
 }
